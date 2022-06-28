02361997630ed147407335b110ea6b4d7b2f5079
DG <it.college.101@gmail.com>
2022-06-28 03:53:03 -0400

    first version of backend app

---
+++ DockerApp-Backend/src/main/java/com/cubic/dockerapp/backend/DockerAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cubic.dockerapp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerAppBackendApplication.class, args);
+	}
+
+}
---
+++ DockerApp-Backend/src/test/java/com/cubic/dockerapp/backend/DockerAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cubic.dockerapp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
