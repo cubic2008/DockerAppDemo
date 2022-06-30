12f4bcafb14faa41c9ddd3cdd552d132a2694561
DG <davidgao2008@yahoo.ca>
2022-06-29 20:48:11 -0400

    revised frontend app with usage of environment variables

--- DockerApp-Frontend/src/app/product.service.ts
+++ DockerApp-Frontend/src/app/product.service.ts
@@ -11,7 +11,7 @@ import { MessageService } from './message.service';
 })
 export class ProductService {
   private products = PRODUCTS
-  private apiUrl = 'http://localhost:8080/api/v1/products';
+  private apiUrl = `${environment.apiURL}/api/v1/products`;
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
--- DockerApp-Frontend/src/environments/environment.prod.ts
+++ DockerApp-Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'http://dockapp-restserver:8080'
 };
