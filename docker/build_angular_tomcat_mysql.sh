d36c9389accc4faefb7ff1b64954220a35893597
DG <davidgao2008@yahoo.ca>
2022-06-30 03:14:30 -0400

    fixed environment/tomcat/context.xml

--- docker/build_angular_tomcat_mysql.sh
+++ docker/build_angular_tomcat_mysql.sh
@@ -5,7 +5,7 @@ export DB_USER=appuser
 export DB_PASSWORD=appPassw0rd
 
 # Step 1: Create images
-sudo docker image build --tag dockerapp-restserver -f sprintboot.df .
+#sudo docker image build --tag dockerapp-restserver -f sprintboot.df .
 sudo docker image build --tag dockerapp-tomcat -f tomcat.df .
 sudo docker image build --tag dockerapp-db -f mysql.df .
 sudo docker image build --tag dockerapp-nginx --build-arg apiUrlHost=$HOSTING_SERVER_NAME --build-arg apiUrlPort=$APP_SERVER_PORT -f nginx.df .
@@ -20,10 +20,10 @@ sudo docker run --rm --name dockerapp-db --network dockerapp-network -e MYSQL_US
 # sleep 20
 
 # Step 4a: Create & Run App Server (SpringBoot) Container
-sudo docker run --rm --name dockerapp-restserver --network dockerapp-network -p $APP_SERVER_PORT:8080 -e DOCKERAPP_DB_USER=$DB_USER -e DOCKERAPP_DB_PASSWORD=$DB_PASSWORD -d dockerapp-restserver
+#sudo docker run --rm --name dockerapp-restserver --network dockerapp-network -p $APP_SERVER_PORT:8080 -e DOCKERAPP_DB_USER=$DB_USER -e DOCKERAPP_DB_PASSWORD=$DB_PASSWORD -d dockerapp-restserver
 
 # Step 4b: Create & Run App Server (SpringMVC/Tomcat) Container
-#sudo docker run --rm --name dockerapp-tomcat --network dockerapp-network -p $APP_SERVER_PORT:8080 -e DOCKERAPP_DB_USER=$DB_USER -e DOCKERAPP_DB_PASSWORD=$DB_PASSWORD -d dockerapp-tomcat
+sudo docker run --rm --name dockerapp-tomcat --network dockerapp-network -p $APP_SERVER_PORT:8080 -e DOCKERAPP_DB_USER=$DB_USER -e DOCKERAPP_DB_PASSWORD=$DB_PASSWORD -d dockerapp-tomcat
 
 # sleep 10
 
