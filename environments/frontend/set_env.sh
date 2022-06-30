c3ff712480e69a05da4740d19fafc7ce15e4f792
DG <davidgao2008@yahoo.ca>
2022-06-29 20:36:02 -0400

    added environment configuration files for docker image builds

---
+++ environments/frontend/set_env.sh
@@ -0,0 +1,4 @@
+echo "export const environment = {
+   apiURL: 'http://${API_URL_HOST}:${API_URL_PORT}',
+   production: true,
+};" > /usr/local/app/DockerApp/DockerApp-Frontend/src/environments/environment.ts
