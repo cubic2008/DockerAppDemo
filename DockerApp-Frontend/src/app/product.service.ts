871119e24e56f6396ae68c25b70034cd8ef5c95c
DG <davidgao2008@yahoo.ca>
2022-06-29 21:40:44 -0400

    revised frontend app with usage of environment variables (fixed bug - 2)

--- DockerApp-Frontend/src/app/product.service.ts
+++ DockerApp-Frontend/src/app/product.service.ts
@@ -5,7 +5,7 @@ import { catchError, map, tap } from 'rxjs/operators';
 
 import { Product, PRODUCTS } from './product';
 import { MessageService } from './message.service';
-import { environment } from '../../environments/environment';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
