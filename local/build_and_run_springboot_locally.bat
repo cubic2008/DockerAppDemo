7985f4549c6b26303f67479b58adc93f7cb1050f
DG <it.college.101@gmail.com>
2022-07-04 13:33:36 -0400

    adding local setup files - v6

--- local/build_and_run_springboot_locally.bat
+++ local/build_and_run_springboot_locally.bat
@@ -3,6 +3,6 @@ set DOCKERAPP_DB_PORT=3306
 set DOCKERAPP_DB_USER=appUser
 set DOCKERAPP_DB_PASSWORD=appPassw0rd
 set DOCKERAPP_DB_NAME=dockerappdb
-cd "../Docker/DockerApp-Backend (SpringBoots)"
+cd "../DockerApp-Backend (SpringBoots)"
 mvn -Dmaven.test.skip package
 java -jar target/docker_app_backend-0.0.1-SNAPSHOT.jar
