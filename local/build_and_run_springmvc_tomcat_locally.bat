718776a6497ec393d14f1901384d19efb22c18b7
DG <it.college.101@gmail.com>
2022-07-04 16:26:20 -0400

    adding local setup files - v12

--- local/build_and_run_springmvc_tomcat_locally.bat
+++ local/build_and_run_springmvc_tomcat_locally.bat
@@ -1,49 +1,9 @@
 @echo off
 
-@REM Step 1: Setup the environment variables for connecting to the database server
-
-@REM echo.
-@REM echo +--------------------------------------------------------------------+
-@REM echo ^|      Set environment variables for connecting MySQL database       ^|
-@REM echo +--------------------------------------------------------------------+
-@REM echo.
-
-@echo on
-@REM set DOCKERAPP_DB_HOST=localhost
-@REM set DOCKERAPP_DB_PORT=3306
-@REM set DOCKERAPP_DB_USER=appUser
-@REM set DOCKERAPP_DB_PASSWORD=appPassw0rd
-@REM set DOCKERAPP_DB_NAME=dockerappdb
-@echo off
-
-
 @REM Step 1: Build and Deploy DockerApp-Backend (SpringMVC)
-echo.
-echo.
-echo.
-echo +--------------------------------------------------------------------+
-echo ^|           Build and Deploy DockerApp-Backend (SpringMVC)           ^|
-echo +--------------------------------------------------------------------+
-echo.
-echo.
-echo.
-
-
-@REM Step 2: Start DockerApp-Backend (SpringMVC/Tomcat) REST API server
 
 cd "../DockerApp-Backend (SpringMVC)"
-@REM mvn -Dmaven.test.skip package
+@REM mvn -Dmaven.test.skip package tomcat7:deploy
 call mvn package tomcat7:deploy
 
-echo.
-echo.
-echo.
-echo +--------------------------------------------------------------------+
-echo ^|     Start DockerApp-Backend (SpringMVC/Tomcat) REST API server     ^|
-echo +--------------------------------------------------------------------+
-echo.
-echo.
-echo.
-
-start java -jar target/docker_app_backend-0.0.1-SNAPSHOT.jar
 cd ../local
