d4eab5068840c55e163039bdedb3c2eec4796888
DG <it.college.101@gmail.com>
2022-07-04 13:51:50 -0400

    adding local setup files - v7

--- local/build_and_run_springboot_locally.bat
+++ local/build_and_run_springboot_locally.bat
@@ -1,7 +1,13 @@
 @echo off
-echo "-------------------------------------------------------------"
-echo "Set environment variables for connecting MySQL database . . ."
-echo "-------------------------------------------------------------"
+echo
+echo
+echo
+echo +--------------------------------------------------------------------+
+echo |      Set environment variables for connecting MySQL database       |
+echo +--------------------------------------------------------------------+
+echo
+echo
+echo
 
 set DOCKERAPP_DB_HOST=localhost
 set DOCKERAPP_DB_PORT=3306
@@ -9,16 +15,28 @@ set DOCKERAPP_DB_USER=appUser
 set DOCKERAPP_DB_PASSWORD=appPassw0rd
 set DOCKERAPP_DB_NAME=dockerappdb
 
-echo "-------------------------------------------"
-echo "Build DockerApp-Backend (SpringBoots) . . ."
-echo "-------------------------------------------"
+echo
+echo
+echo
+echo +--------------------------------------------------------------------+
+echo |              Build DockerApp-Backend (SpringBoots)                 |
+echo +--------------------------------------------------------------------+
+echo
+echo
+echo
 
 cd "../DockerApp-Backend (SpringBoots)"
 @REM mvn -Dmaven.test.skip package
 call mvn package
 
-echo "-----------------------------------------------------------"
-echo "Start DockerApp-Backend (SpringBoots) REST API server . . ."
-echo "-----------------------------------------------------------"
+echo
+echo
+echo
+echo +--------------------------------------------------------------------+
+echo |       Start DockerApp-Backend (SpringBoots) REST API server        |
+echo +--------------------------------------------------------------------+
+echo
+echo
+echo
 
 java -jar target/docker_app_backend-0.0.1-SNAPSHOT.jar
